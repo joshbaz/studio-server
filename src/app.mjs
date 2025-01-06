5b88b73ac94dacc19e7789eb5d33302053399eb5
Newton Mbugua <mymbugua@gmail.com>
2025-01-06 11:31:40 +0000

    feat: added stream url to origin

--- src/app.mjs
+++ src/app.mjs
@@ -83,13 +83,13 @@ export default function customizeApp(app) {
     const corsOptions = {
         origin: [
             env.CLIENT_URL,
-            '*',
             'http://localhost:8081',
             'http://192.168.0.184:4500',
             'http://localhost:5173',
             'https://staging.nyatimotionpictures.com',
             'https://nyatimotionpictures.com',
             'https://studio.nyatimotionpictures.com',
+            'https://stream.nyatimotionpictures.com',
         ],
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         preflightContinue: false,

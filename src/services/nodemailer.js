b29278f6146809c69932124057f56ff572b8a45f
joshbaz <jkimbareeba@yahoo.com>
2025-07-01 12:27:07 +0300

    changed transporter port

--- src/services/nodemailer.js
+++ src/services/nodemailer.js
@@ -1,15 +1,7 @@
 import nodemailer from 'nodemailer';
 import { env } from '@/env.mjs';
 
-const transporter = nodemailer.createTransport({
-   host: env.NODE_MAILER_HOST,
-   port: 465,
-   secure: true,
-   auth: {
-      user: env.NODE_MAILER_USERCRED,
-      pass: env.NODE_MAILER_PASSCRED,
-   },
-});
+
 
 /**
  * @name sendMail
@@ -18,6 +10,15 @@ const transporter = nodemailer.createTransport({
  */
 export const sendMail = async (mailOptions) => {
    try {
+      const transporter = nodemailer.createTransport({
+         host: env.NODE_MAILER_HOST,
+         port: 465,
+         secure: true,
+         auth: {
+            user: env.NODE_MAILER_USERCRED,
+            pass: env.NODE_MAILER_PASSCRED,
+         },
+      });
       const response = await transporter.sendMail(mailOptions);
       console.log('Message sent: %s', response.messageId);
       return response;

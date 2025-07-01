dfae5483f317e2f741a7026b2a298680dfe210a7
joshbaz <jkimbareeba@yahoo.com>
2025-07-01 12:22:35 +0300

    changed transporter port

--- src/services/nodemailer.js
+++ src/services/nodemailer.js
@@ -3,7 +3,7 @@ import { env } from '@/env.mjs';
 
 const transporter = nodemailer.createTransport({
    host: env.NODE_MAILER_HOST,
-   port: 587,
+   port: 465,
    secure: true,
    auth: {
       user: env.NODE_MAILER_USERCRED,
